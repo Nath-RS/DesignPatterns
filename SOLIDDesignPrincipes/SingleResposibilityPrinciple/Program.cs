89261f36993348a82541eb6deebdc1e22f6fed8b
Nath-RS <78105588+Nath-RS@users.noreply.github.com>
2025-07-18 14:11:11 -0300

    Single Solid Principe

---
+++ SOLIDDesignPrincipes/SingleResposibilityPrinciple/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResposibilityPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class page
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        override public string ToString()
+        {
+            return $"{Title} - {Content}";
+        }
+    }
+
+
+
+}
---
+++ SOLIDDesignPrincipes/SingleResposibilityPrinciple/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("SingleResposibilityPrinciple")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SingleResposibilityPrinciple")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("44e8763b-5000-438a-95a1-92df2926009d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
